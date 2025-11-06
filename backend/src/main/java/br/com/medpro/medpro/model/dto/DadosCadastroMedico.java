e817c1bc9dc6e6ea9647cbdd0791d89f950fbd2b
CaioDalNegro <caiodalnegro@gmail.com>
2025-11-05 22:41:28 -0300

    NEW: Adicionando migrations

--- backend/src/main/java/br/com/medpro/medpro/model/dto/DadosCadastroMedico.java
+++ backend/src/main/java/br/com/medpro/medpro/model/dto/DadosCadastroMedico.java
@@ -1,7 +1,17 @@
 package br.com.medpro.medpro.model.dto;
 
 import br.com.medpro.medpro.model.enums.Especialidade;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+    @NotBlank String nome, 
+    @NotBlank String email,
+    @NotBlank String telefone, 
+    @NotBlank @Pattern(regexp = "\\d{4,6}") String crm, 
+    @NotNull Especialidade especialidade, 
+    @NotNull @Valid DadosEndereco endereco) {
 
 }
